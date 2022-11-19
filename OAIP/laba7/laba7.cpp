3ea6bf85f14c19e6ad8f648d0cda145e37c3edc0
wuttang <volozarkhip@gmail.com>
2022-11-19 19:16:39 +0300

    tmois labw2 fixed.

---
+++ OAIP/laba7/laba7.cpp
@@ -0,0 +1,18 @@
+// variant 4
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[100];
+    char shifr[10]= "ла";
+    gets(str);
+    for (int i = 0; i<strlen(str); i++) {
+        if (str[i] == 'd' || str[i] == 'l' || str[i] == 'd' || str[i] == 'c' || str[i] == 'm') {
+            
+        }
+    }
+    puts(str);
+}
