d018a737f6c8f01a141f2c9469086b774f0d8b87
wuttang <volozarkhip@gmail.com>
2023-03-15 22:51:48 +0300

    added OAIP/lab3+lab4 & TMOIS/lab3

--- OAIP/sem2/lab3/lab3.cpp
+++ OAIP/sem2/lab3/lab3.cpp
@@ -1,44 +1,89 @@
 #include <iostream>
-#include <stack>
 
 using namespace std;
 
-void swapExtremes(stack<int>& s) {
-    if (s.empty()) return;
-    int top = s.top();
-    s.pop();
-    if (s.empty()) {
-        s.push(top);
-        return;
+struct Node {
+    int data;
+    Node* next;
+};
+
+struct Stack {
+    Node* head;
+    Node* tail;
+};
+
+void push(Stack* stack, int data) {
+    Node* node = new Node;
+    node->data = data;
+    node->next = stack->head;
+    stack->head = node;
+    if (!stack->tail) {
+        stack->tail = node;
     }
-    int bottom = s.top();
-    s.pop();
-    s.push(top);
-    s.push(bottom);
 }
 
-void printStack(stack<int> s) {
-    while (!s.empty()) {
-        cout << s.top() << " ";
-        s.pop();
+int pop(Stack* stack) {
+    if (!stack->head) {
+        return -1; // если стек пустой
+    }
+    int data = stack->head->data;
+    Node* node = stack->head;
+    stack->head = stack->head->next;
+    if (!stack->head) {
+        stack->tail = NULL;
+    }
+    delete node;
+    return data;
+}
+
+void swap_first_last(Stack* stack) {
+    if (!stack->head || !stack->head->next) {
+        return; // если в стеке меньше двух элементов
+    }
+    int first_data = stack->head->data;
+    int last_data = stack->tail->data;
+    stack->head->data = last_data;
+    stack->tail->data = first_data;
+}
+
+void print_stack(Stack* stack) {
+    Node* node = stack->head;
+    while (node) {
+        cout << node->data << " ";
+        node = node->next;
     }
     cout << endl;
 }
 
+double num_check()
+{   
+    int n;
+    do {
+        cin >> n;
+        if (!cin || cin.get() != '\n') {
+            cin.clear();
+            cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+            cout << "Повторите попытку: ";
+        } else {
+            return n;
+        }
+    } while (true);
+}
+
 int main() {
-    stack<int> s;
-    int n, num;
-    cout << "Enter the number of elements to add: ";
-    cin >> n;
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        s.push(num);
+    Stack stack = { NULL, NULL };
+    int size = 0, elem = 0;
+
+    cout << "Введите размер стека: ";
+    size = num_check();
+    for (int i = 0; i < size; i++){
+        cout << "Введите элемент стека: ";
+        elem = num_check();
+        push(&stack, elem);
     }
-    cout << "Stack elements: ";
-    printStack(s);
-    swapExtremes(s);
-    cout << "Stack after swapping extremes: ";
-    printStack(s);
+    print_stack(&stack);
+    
+    swap_first_last(&stack);
+    print_stack(&stack);
     return 0;
 }
---
+++ OAIP/sem2/lab4/lab4.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+};
+
+struct Queue {
+    Node* head;
+    Node* tail;
+};
+
+void push(Queue* queue, int data) {
+    Node* node = new Node;
+    node->data = data;
+    node->next = NULL;
+    node->prev = queue->tail;
+    if (queue->tail) {
+        queue->tail->next = node;
+    } else {
+        queue->head = node;
+    }
+    queue->tail = node;
+}
+
+int pop(Queue* queue) {
+    if (!queue->head) {
+        return -1; // если очередь пустая
+    }
+    int data = queue->head->data;
+    Node* node = queue->head;
+    queue->head = queue->head->next;
+    if (queue->head) {
+        queue->head->prev = NULL;
+    } else {
+        queue->tail = NULL;
+    }
+    delete node;
+    return data;
+}
+
+void swap_first_last(Queue* queue) {
+    if (!queue->head || !queue->head->next) {
+        return; // если в очереди меньше двух элементов
+    }
+    int first_data = queue->head->data;
+    int last_data = queue->tail->data;
+    queue->head->data = last_data;
+    queue->tail->data = first_data;
+}
+
+void print_queue(Queue* queue) {
+    Node* node = queue->head;
+    while (node) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+double num_check()
+{   
+    int n;
+    do {
+        cin >> n;
+        if (!cin || cin.get() != '\n') {
+            cin.clear();
+            cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+            cout << "Повторите попытку: ";
+        } else {
+            return n;
+        }
+    } while (true);
+}
+
+int main()
+{
+    Queue queue = {NULL, NULL};
+     int size = 0, elem = 0;
+
+    cout << "Введите размер очереди: ";
+    size = num_check();
+    for (int i = 0; i < size; i++){
+        cout << "Введите элемент очереди: ";
+        elem = num_check();
+        push(&queue, elem);
+    }
+    print_queue(&queue);
+    
+    swap_first_last(&queue);
+    print_queue(&queue);
+    return 0;
+}
