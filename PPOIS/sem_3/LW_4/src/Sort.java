71d5d229acb9a70f1d6a2f179315de3723c38542
wuttang. <volozarkhip@gmail.com>
2023-11-20 21:39:59 +0300

    LW_4 PPOIS changed

--- PPOIS/sem_3/LW_4/src/Sort.java
+++ PPOIS/sem_3/LW_4/src/Sort.java
@@ -1,35 +1,46 @@
 import java.util.Comparator;
+import java.util.List;
 
 public class Sort {
-    public static <T extends Comparable<T>> void quickSort(T[] array) {
+    public static <T extends Comparable<T>> void quickSort(T[] array) throws Exception {
         if (array == null || array.length == 0) {
-            return;
+            throw new Exception("Element is null");
         }
         quickSort(array, 0, array.length - 1);
     }
 
-    private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high) {
-        if (low < high) {
+    private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high) throws Exception {
+        if (array == null || low < 0 || high >= array.length || low > high) {
+            return;
+        } else {
             int partitionIndex = partition(array, low, high);
             quickSort(array, low, partitionIndex - 1);
             quickSort(array, partitionIndex + 1, high);
         }
     }
 
-    private static <T extends Comparable<T>> int partition(T[] array, int low, int high) {
+    private static <T extends Comparable<T>> int partition(T[] array, int low, int high) throws Exception {
+        if (array == null || array.length == 0) {
+            throw new Exception("Element is null");
+        }
         T pivot = array[high];
         int i = low - 1;
         for (int j = low; j < high; j++) {
-            if (array[j].compareTo(pivot) <= 0) {
+            if (array[j].compareTo(pivot) < 0) {
                 i++;
-                swap(array, i, j);
+                if (i != j) {
+                    swap(array, i, j);
+                }
             }
         }
         swap(array, i + 1, high);
         return i + 1;
     }
 
-    private static <T> void swap(T[] array, int i, int j) {
+    private static <T> void swap(T[] array, int i, int j) throws Exception {
+        if (array == null || array.length == 0) {
+            throw new Exception("Element is null");
+        }
         T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -42,31 +53,111 @@ public class Sort {
         System.out.println();
     }
 
-    public static <T> void quickSort(T[] array, Comparator<T> comparator) {
+    public static <T> void quickSort(T[] array, Comparator<T> comparator) throws Exception {
         if (array == null || array.length == 0) {
-            return;
+            throw new Exception("Element is null");
         }
         quickSort(array, 0, array.length - 1, comparator);
     }
 
-    private static <T> void quickSort(T[] array, int low, int high, Comparator<T> comparator) {
-        if (low < high) {
-            int partitionIndex = partition(array, low, high, comparator);
-            quickSort(array, low, partitionIndex - 1, comparator);
-            quickSort(array, partitionIndex + 1, high, comparator);
+    private static <T> void quickSort(T[] array, int low, int high, Comparator<T> comparator) throws Exception {
+        if (array == null || low < 0 || high >= array.length || low > high || comparator == null) {
+            return;
         }
+        int partitionIndex = partition(array, low, high, comparator);
+        quickSort(array, low, partitionIndex - 1, comparator);
+        quickSort(array, partitionIndex + 1, high, comparator);
     }
 
-    private static <T> int partition(T[] array, int low, int high, Comparator<T> comparator) {
+    private static <T> int partition(T[] array, int low, int high, Comparator<T> comparator) throws Exception {
+        if (array == null || array.length == 0) {
+            throw new Exception("Element is null");
+        }
         T pivot = array[high];
         int i = low - 1;
         for (int j = low; j < high; j++) {
-            if (comparator.compare(array[j], pivot) <= 0) {
+            if (comparator.compare(array[j], pivot) < 0) {
                 i++;
                 swap(array, i, j);
             }
         }
         swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    public static <T extends Comparable<T>> void quickSort(List<T> list) throws Exception {
+        if (list == null || list.isEmpty()) {
+            throw new Exception("Element is null");
+        }
+        quickSort(list, 0, list.size() - 1);
+    }
+
+    private static <T extends Comparable<T>> void quickSort(List<T> list, int low, int high) throws Exception {
+        if (list == null || list.isEmpty() || low < 0 || high >= list.size() || low > high) {
+            return;
+        }
+        int partitionIndex = partition(list, low, high);
+        quickSort(list, low, partitionIndex - 1);
+        quickSort(list, partitionIndex + 1, high);
+
+    }
+
+    private static <T extends Comparable<T>> int partition(List<T> list, int low, int high) throws Exception {
+        if (list == null || list.isEmpty()) {
+            throw new Exception("Element is null");
+        }
+        T pivot = list.get(high);
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (list.get(j).compareTo(pivot) < 0) {
+                i++;
+                swap(list, i, j);
+            }
+        }
+        swap(list, i + 1, high);
+        return i + 1;
+    }
+
+    private static <T> void swap(List<T> list, int i, int j) throws Exception {
+        if (list == null || list.isEmpty()) {
+            throw new Exception("Element is null");
+        }
+        T temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    public static <T> void quickSort(List<T> list, Comparator<T> comparator) throws Exception {
+        if (list == null || list.isEmpty() || comparator == null) {
+            throw new Exception("Element is null");
+        }
+
+        quickSort(list, 0, list.size() - 1, comparator);
+    }
+
+    private static <T> void quickSort(List<T> list, int low, int high, Comparator<T> comparator) throws Exception {
+        if (list == null || list.isEmpty() || low < 0 || high >= list.size() || low > high || comparator == null) {
+            return;
+        }
+        int partitionIndex = partition(list, low, high, comparator);
+        quickSort(list, low, partitionIndex - 1, comparator);
+        quickSort(list, partitionIndex + 1, high, comparator);
+
+    }
+
+    private static <T> int partition(List<T> list, int low, int high, Comparator<T> comparator) throws Exception {
+        if (list == null || list.isEmpty()) {
+            throw new Exception("Element is null");
+        }
+        T pivot = list.get(high);
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (comparator.compare(list.get(j), pivot) < 0) {
+                i++;
+                swap(list, i, j);
+            }
+        }
+        swap(list, i + 1, high);
         return i + 1;
     }
 }
--- PPOIS/sem_3/LW_4/src/SortTest.java
+++ PPOIS/sem_3/LW_4/src/SortTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 import org.junit.Assert;
+import java.util.List;
+import java.util.ArrayList;
 
 public class SortTest {
     @Test
-    public void PersonSortingTest() {
+    public void PersonSortingTest() throws Exception {
         Person[] people = {
                 new Person("Lars", 58),
                 new Person("James", 63),
@@ -20,7 +22,7 @@ public class SortTest {
     }
 
     @Test
-    public void CarSortingTest() {
+    public void CarSortingTest() throws Exception {
         Car[] cars = {
                 new Car("Toyota", 250),
                 new Car("BMW", 350),
@@ -38,7 +40,7 @@ public class SortTest {
     }
 
     @Test
-    public void PersonSortingWithComparatorTest() {
+    public void PersonSortingWithComparatorTest() throws Exception {
         Person[] people = {
                 new Person("Till", 60),
                 new Person("Richard", 48),
@@ -56,7 +58,7 @@ public class SortTest {
     }
 
     @Test
-    public void CarSortingWithComparatorTest() {
+    public void CarSortingWithComparatorTest() throws Exception {
         Car[] cars = {
                 new Car("Mercedes-Benz", 380),
                 new Car("Nissan", 320),
@@ -73,7 +75,7 @@ public class SortTest {
     }
 
     @Test
-    public void IntSorterTest() {
+    public void IntSorterTest() throws Exception {
         Integer[] num = {9, 4, 1, 8, 6};
         Sort.quickSort(num);
 
@@ -82,5 +84,35 @@ public class SortTest {
         Assert.assertArrayEquals(numTest, num);
     }
 
+    @Test
+    public void VectorSortTest() throws Exception {
+        List<String> vector = new ArrayList<>();
+        vector.add("jingle");
+        vector.add("bells");
+        vector.add("xmas");
 
+        List<String> vectorTest = new ArrayList<>();
+        vectorTest.add("bells");
+        vectorTest.add("jingle");
+        vectorTest.add("xmas");
+
+        Sort.quickSort(vector);
+        Assert.assertEquals(vectorTest,vector);
+    }
+
+    @Test
+    public void VectorComparatorSortTest() throws Exception {
+        List<Car> vector = new ArrayList<>();
+        vector.add(new Car("Acura", 350));
+        vector.add(new Car("Toyota", 300));
+        vector.add(new Car("Lexus", 380));
+
+        List<Car> vectorTest = new ArrayList<>();
+        vectorTest.add(new Car("Toyota", 300));
+        vectorTest.add(new Car("Acura", 350));
+        vectorTest.add(new Car("Lexus", 380));
+
+        Sort.quickSort(vector, new CarMaxSpeedComparator());
+        Assert.assertEquals(vectorTest,vector);
+    }
 }
